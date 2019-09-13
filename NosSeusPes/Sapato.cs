9be50e2b95685f9338e4c113f4df118723352bb8
Bruno Hoffmann de Mattos <bruno.mattos2907@yahoo.com.br>
2019-09-12 22:13:26 -0300

    Adicionado as entidades.

---
+++ NosSeusPes/Sapato.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NosSeusPes
+{
+    public class Sapato
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public bool Cadarco { get; set; }
+        public string Material { get; set; }
+        public string Cor { get; set; }
+        public decimal Preco { get; set; }
+        public byte[] Fotografia { get; set; }
+
+    }
+}
