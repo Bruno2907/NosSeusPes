34ad2b5ebac5ca26d9686343373b44ffe4d25ab4
Abel-Leal <abelsouzaleal88@outlook.com>
2019-09-30 22:06:50 -0300

    Telas de Sapato e Estoque Concluídas.

---
+++ NosSeusPesWPF/ViewModel/EstoqueViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using NosSeusPes;
+
+namespace NosSeusPesWPF.ViewModel
+{
+    class EstoqueViewModel
+    {
+        public ObservableCollection<Estoque> Estoques { get; set; }
+        public Model model;
+        public EstoqueViewModel()
+        {
+            model = new Model();
+            Estoques = new ObservableCollection<Estoque>(model.Estoques.ToList());
+            Sapatos = new ObservableCollection<Sapato>(model.Sapatos.ToList());
+            EstoqueSelecionado = new Estoque();
+        }
+        public Estoque EstoqueSelecionado { get; set; }
+        public void DeletarEstoque(int ID)
+        {
+            Estoque e;
+            e = Estoques.Where(es => es.Id == ID).FirstOrDefault();
+            Estoques.Remove(e);
+            model.Estoques.Remove(e);
+            model.SaveChanges();
+        }
+
+        public void SalvarNovoEstoque()
+        {
+            Estoque e = EstoqueSelecionado;
+            Estoques.Add(e);
+            model.Estoques.Add(e);
+            EstoqueSelecionado = new Estoque();
+            model.SaveChanges();
+        }
+
+        public ObservableCollection<Sapato> Sapatos { get; set; }
+    }
+}
